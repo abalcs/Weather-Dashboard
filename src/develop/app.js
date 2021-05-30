4f377f957c9ebd9e509c571c581659b083736cbe
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-05-30 15:21:17 -0400

    refactoring & working on uv color logic

--- src/develop/app.js
+++ src/develop/app.js
@@ -1,6 +1,6 @@
+//Setting the global variables
 let latitude;
 let longitude;
-let city;
 let cityChoice;
 let cityArray = [];
 let searchInput = $("#searchInput")
@@ -8,12 +8,14 @@ let searchInput = $("#searchInput")
 searchButton.addEventListener("click", checkCityEntry)
 
 function checkCityEntry() {
-    city = searchInput.val().replace(/\s/g, "");
+    //Sets search value to variable & removes the empty spaces between characters
+    let city = searchInput.val().replace(/\s/g, "");
 
     if (city == "") {
-        alert('Please enter a city to check the weather!')
+        alert('No city? No weather!')
 
     } else {
+        //Converts the input to uppercase, runs getOneCall function, then stores in ls
         if (city.includes(",")) {
             cityChoice = city.toUpperCase();
             getCoordinates()
@@ -30,6 +32,7 @@ function checkCityEntry() {
 
 function getCoordinates() {
     let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityChoice},USA&Appid=d788f32e8b9da745fbd42aba6ed8176a&units=imperial`;
+    //Retrieving data from api, converting to js
     return fetch(weatherUrl)
         .then(function (response1) {
             if (response1.ok) {
@@ -39,6 +42,7 @@ function getCoordinates() {
                 throw new Error('Failed to get coordinates')
             }
         })
+         //Setting coordinates from api to variables & returning data as an object
         .then((data) => {
             latitude = data.coord.lat;
             longitude = data.coord.lon;
@@ -52,27 +56,28 @@ function getCoordinates() {
 }
 
 function getOneCall(input) {
-    // let { latitude, longitude } = input;
+    // Retrreving the api data from onecall & setting it to a variable
     let oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&Appid=d788f32e8b9da745fbd42aba6ed8176a&units=imperial";
     return fetch(oneCallUrl)
         .then(function (response) {
             if (response.ok) {
+                // Parsing fetched data to JS then looping over 5 days to display data for the 5-day-forecast
                 return response.json().then(function (data) {
                     for (let i = 1; i < 6; i++) {
                         let dayTitleCard = $(`#day${i}Title`);
-                        dayTitleCard.text(dayjs.unix(data.daily[i].dt).format('ddd    MM/DD'));
+                        dayTitleCard.text(dayjs.unix(data.daily[i].dt).format('ddd    MM/DD'));  //Using dayjs to display dates
                         let dayIconCard = $(`#day${i}Icon`);
                         dayIconCard.attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
                         let dayDescCard = $(`#day${i}Desc`);
                         dayDescCard.html(`Lo: ${parseInt(data.daily[i].temp.min)}\xB0 F</br>Hi: ${parseInt(data.daily[i].temp.max)}\xB0 F</br>Humid: ${data.daily[i].humidity} %</br>Wind: ${data.daily[i].wind_speed} MPH `);
                     }
 
-                    let weatherTitle = $("#weatherTitle");
+                    let weatherTitle = $("#weatherTitle");  //Connecting to the DOM element to set the city choice to the section title
                     weatherTitle.text(`5-Day Forecast: ${cityChoice}`);
                     let currentCity = $("#currentCity");
                     currentCity.text(`Current Forecast: ${cityChoice}`);
                     let curDateCard = $("#currentTitle");
-                    curDateCard.text(dayjs.unix(data.current.dt).format('ddd, MMM DD, YYYY'));
+                    curDateCard.text(dayjs.unix(data.current.dt).format('ddd, MMM DD, YYYY'));  //Using dayjs for date again
                     let curIconCard = $("#currentIcon");
                     curIconCard.attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
                     let curTempCard = $("#currentTemp");
@@ -82,17 +87,17 @@ function getOneCall(input) {
                     let curUviCard = $("#currentUvi");
                     curUviCard.text(`UV Index: ${data.current.uvi}`);
                     let curWindCard = $("#currentWind");
-                    curWindCard.text(`Wind Speed: ${data.current.wind_speed}`);
+                    curWindCard.text(`Wind Speed: ${data.current.wind_speed} MPH`);
                     let curHumidCard = $("#currentHumidity");
-                    curHumidCard.text(`Humidity: ${data.current.humidity}`)
-
-                    if (parseInt(data.current.uvi) < 3) {
+                    curHumidCard.text(`Humidity: ${data.current.humidity}%`)
+                    
+                    //Setting UV to display color coding based on conditions
+                    if (parseInt(data.current.uvi) <= 3) {
                         $("#currentUvi").addClass("uviGood")
-                    } else if (parseInt(data.current.uvi) < 6) {
+                    } else if (parseInt(data.current.uvi) > 3 ) {
                         $("#currentUvi").addClass("uviModerate")
-                    } else if (parseInt(data.current.uvi) < 8) {
+                    } else if (parseInt(data.current.uvi) > 6 ) {
                         $("#currentUvi").addClass("uviBad")
-
                     } else {
                         $("#currentUvi").addClass("uviExtreme")
                     }
@@ -104,21 +109,23 @@ function getOneCall(input) {
 };
 
 function storeCity() {
-    
+    // retrieves the current array and parses it to JS
     let cityArray = JSON.parse(localStorage.getItem("cityArray")) || [];
-
-    if (cityArray.length > 10) {
+    // when the array is more than 10 cities, push the newest search to the front and remove the 11th
+    if (cityArray.length > 9) {
         cityArray.splice(0, 1);
     }
-
+    //checks if the city is already in the array and does not push it again if so, otherwise it will
     if (!cityArray.includes(cityChoice)) {
             cityArray.push(cityChoice)
+            // sets the updated array as a JSON object
             localStorage.setItem("cityArray", JSON.stringify(cityArray));
             cityList();
         }  
     }
 
 function cityList() {
+    //removes all child nodes and content from the list-group element
     let listEl = $(".list-group").empty();
     cityArray = JSON.parse(localStorage.getItem("cityArray"));
     if (cityArray) {
@@ -126,9 +133,9 @@ function cityList() {
                 let btn = $("<button>", {
                 text: city
             })
-
-            let li = $('<li>').append(btn)
-            listEl.append(li);
+            // places the most recent searched city at the top of the displayed list
+            let li = $('<li>').prepend(btn)
+            listEl.prepend(li);
             btn.on ('click', function() {
                 searchInput.val(city)
                 checkCityEntry();
