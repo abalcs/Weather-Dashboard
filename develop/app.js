0265217f678310f681f4fba8a94da83724328549
Alan Balcom <abalcom23@gmail.com>
2023-03-11 18:25:05 -0500

    light updates to syntax

--- develop/app.js
+++ develop/app.js
@@ -41,6 +41,7 @@ function getCoordinates() {
         })
          //Setting coordinates from api to variables & returning data as an object
         .then((data) => {
+            console.log(data)
             latitude = data.coord.lat;
             longitude = data.coord.lon;
             return {
@@ -54,52 +55,56 @@ function getOneCall(input) {
     // Retrreving the api data from onecall & setting it to a variable
     let oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&Appid=d788f32e8b9da745fbd42aba6ed8176a&units=imperial";
     return fetch(oneCallUrl)
-        .then(function (response) {
-            if (response.ok) {
-                // Parsing fetched data to JS then looping over 5 days to display data for the 5-day-forecast
-                return response.json().then(function (data) {
-                    for (let i = 1; i < 6; i++) {
-                        let dayTitleCard = $(`#day${i}Title`);
-                        dayTitleCard.text(dayjs.unix(data.daily[i].dt).format('ddd    MM/DD'));  //Using dayjs to display dates
-                        let dayIconCard = $(`#day${i}Icon`);
-                        dayIconCard.attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
-                        let dayDescCard = $(`#day${i}Desc`);
-                        dayDescCard.html(`Lo: ${parseInt(data.daily[i].temp.min)}\xB0 F</br>Hi: ${parseInt(data.daily[i].temp.max)}\xB0 F</br>Humid: ${data.daily[i].humidity} %</br>Wind: ${data.daily[i].wind_speed} MPH `);
-                    }
+    .then(function (response) {
+        if (response.ok) {
+            // Parsing fetched data to JS then looping over 5 days to display data for the 5-day-forecast
+            return response.json().then(function (data) {
+                console.log(data)
+                for (let i = 1; i < 6; i++) {
+                    let dayTitleCard = $(`#day${i}Title`);
+                    dayTitleCard.text(dayjs.unix(data.daily[i].dt).format('ddd    MM/DD'));  //Using dayjs to display dates
+                    let dayIconCard = $(`#day${i}Icon`);
+                    dayIconCard.attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
+                    let dayDescCard = $(`#day${i}Desc`);
+                    dayDescCard.html(`Lo: ${parseInt(data.daily[i].temp.min)}\xB0 F</br>
+                                    Hi: ${parseInt(data.daily[i].temp.max)}\xB0 F</br>
+                                    Humid: ${data.daily[i].humidity} %</br>
+                                    Wind: ${data.daily[i].wind_speed} MPH `);
+                }
 
-                    let weatherTitle = $("#weatherTitle");  //Connecting to the DOM element to set the city choice to the section title
-                    weatherTitle.text(`5-Day Forecast: ${cityChoice}`);
-                    let currentCity = $("#currentCity");
-                    currentCity.text(`Current Forecast: ${cityChoice}`);
-                    let curDateCard = $("#currentTitle");
-                    curDateCard.text(dayjs.unix(data.current.dt).format('ddd, MMM DD, YYYY'));  //Using dayjs for date again
-                    let curIconCard = $("#currentIcon");
-                    curIconCard.attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
-                    let curTempCard = $("#currentTemp");
-                    curTempCard.text(`Temp: ${data.current.temp}`);
-                    let curCondCard = $("#currentDesc");
-                    curCondCard.text(`Conditions: ${data.current.weather[0].main}`);
-                    let curUviCard = $("#currentUvi");
-                    curUviCard.text(`UV Index: ${data.current.uvi}`);
-                    let curWindCard = $("#currentWind");
-                    curWindCard.text(`Wind Speed: ${data.current.wind_speed} MPH`);
-                    let curHumidCard = $("#currentHumidity");
-                    curHumidCard.text(`Humidity: ${data.current.humidity}%`)
-                    
-                    //Setting UV to display color coding based on conditions
-                    const uvi = parseInt(data.current.uvi);
-                    if (uvi <=3) {
-                        $("#currentUvi").addClass("uviGood")
-                    } else if (uvi >3 && uvi < 6) {
-                        $("#currentUvi").addClass("uviModerate")
-                    } else if (uvi >= 6 && uvi <8 ) {
-                        $("#currentUvi").addClass("uviBad")
-                    } else {
-                        $("#currentUvi").addClass("uviExtreme")
-                    } 
-                }) 
-            } 
-        })
+                let weatherTitle = $("#weatherTitle");  //Connecting to the DOM element to set the city choice to the section title
+                weatherTitle.text(`5-Day Forecast: ${cityChoice}`);
+                let currentCity = $("#currentCity");
+                currentCity.text(`Current Forecast: ${cityChoice}`);
+                let curDateCard = $("#currentTitle");
+                curDateCard.text(dayjs.unix(data.current.dt).format('ddd, MMM DD, YYYY'));  //Using dayjs for date again
+                let curIconCard = $("#currentIcon");
+                curIconCard.attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
+                let curTempCard = $("#currentTemp");
+                curTempCard.text(`Temp: ${data.current.temp}`);
+                let curCondCard = $("#currentDesc");
+                curCondCard.text(`Conditions: ${data.current.weather[0].main}`);
+                let curUviCard = $("#currentUvi");
+                curUviCard.text(`UV Index: ${data.current.uvi}`);
+                let curWindCard = $("#currentWind");
+                curWindCard.text(`Wind Speed: ${data.current.wind_speed} MPH`);
+                let curHumidCard = $("#currentHumidity");
+                curHumidCard.text(`Humidity: ${data.current.humidity}%`)
+                
+                //Setting UV to display color coding based on conditions
+                const uvi = parseInt(data.current.uvi);
+                if (uvi <=3) {
+                    $("#currentUvi").addClass("uviGood")
+                } else if (uvi >3 && uvi < 6) {
+                    $("#currentUvi").addClass("uviModerate")
+                } else if (uvi >= 6 && uvi <8 ) {
+                    $("#currentUvi").addClass("uviBad")
+                } else {
+                    $("#currentUvi").addClass("uviExtreme")
+                } 
+            }) 
+        } 
+    })
 };
 
 function storeCity() {
@@ -111,12 +116,12 @@ function storeCity() {
     }
     //checks if the city is already in the array and does not push it again if so, otherwise it will
     if (!cityArray.includes(cityChoice)) {
-            cityArray.push(cityChoice)
-            // sets the updated array as a JSON object
-            localStorage.setItem("cityArray", JSON.stringify(cityArray));
-            cityList();
-        }  
-    }
+        cityArray.push(cityChoice)
+        // sets the updated array as a JSON object
+        localStorage.setItem("cityArray", JSON.stringify(cityArray));
+        cityList();
+    }  
+}
 
 function cityList() {
     //removes all child nodes and content from the list-group element
@@ -140,4 +145,3 @@ function cityList() {
 };
 
 cityList();
-
